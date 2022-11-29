873a90d9de934bd17554df32021806c0a5c53000
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-29 22:22:34 +0300

    Linters Solve 1st attempt

--- src/spec/book_spec.rb
+++ src/spec/book_spec.rb
@@ -1,12 +1,12 @@
 require_relative './../app/book'
 
 describe Book do
-  context 'when it's initialized' do
-    it 'should create a book instance' do
-      book = Book.new('Book title', 'publisher', '2021-05-12')
-      expect(book.cover_state).to eq('Book title')
-      expect(book.publisher).to eq('publisher')
-      expect(book.can_be_archived?).to eq(false)
+  context 'when initialized' do
+    it 'Should create a new book instance' do
+      book = Book.new('Ruby Programming', 'Baraka Danny', '2015/10/10')
+      expect(book.cover_state).to eq('Ruby Programming')
+      expect(book.publisher).to eq('Baraka Danny')
+      expect(book.can_be_archived?).to be(false)
     end
   end
 end

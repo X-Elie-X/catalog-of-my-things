01b2300c931b31dd1185871443820e64675f08ca
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-12-01 19:33:56 +0200

    Update schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -23,3 +23,16 @@ CREATE TABLE books (
     cover_state VARCHAR(255),
     FOREIGN KEY (id) REFERENCES item(id),
 );
+
+CREATE TABLE author(
+id INT  generate always as identity,
+full_name VARCHAR(60),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE games(
+id INT,
+player  BOOLEAN,
+last_palayed_at DATE,
+FOREIGN KEY (id) REFERENCES item(id)
+);
